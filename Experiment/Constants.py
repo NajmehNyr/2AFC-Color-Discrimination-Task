4e2d0e836ec3e01f9c90c30a709630aa9da02297
NajmehNyr <najmehnayyer@gmail.com>
2025-09-06 01:10:19 +0545

    Update Constants.py

--- Experiment/Constants.py
+++ Experiment/Constants.py
@@ -5,6 +5,7 @@ DISPSIZE = (1920, 1080)
 FGC = (0, 0, 0) #grey
 BGC = (-1, -1, -1) #black
 adaptcolor = (-1, -1, 1)
+rand = np.linspace(0,0.7,10) #Generating experiment's main colors
 
 # number of colors & representation
 NumOfColors = 20
@@ -14,5 +15,3 @@ reps = 20
 # adaptation time
 long =  float(30)
 short = float(10)
-
-
